de4f37da6b92857cc3d01fb9892113da71d945c6
hoteldle <161709207+hoteldle@users.noreply.github.com>
2024-03-08 07:47:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -168,9 +168,9 @@ function autocomplete(inp, arr) {
             b = document.createElement("DIV");
             b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
             b.innerHTML += arr[i].substr(val.length);
-            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                b.addEventListener("click", function(e) {
-                inp.value = this.getElementsByTagName("input")[0].value;
+            b.innerHTML += "<input type='hidden' value=" + encodeURIComponent(arr[i]) + ">";
+            b.addEventListener("click", function(e) {
+                inp.value = decodeURIComponent(this.getElementsByTagName("input")[0].value);
                 closeAllLists();
             });
             a.appendChild(b);
